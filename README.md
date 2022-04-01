096a656eac0c337ecdd7fd85d7bea9fda15397a1
Son Luong Ngoc <sluongng@gmail.com>
2022-04-01 22:56:13 +0700

    brain dump

---
+++ README.md
@@ -0,0 +1,41 @@
+# NoGo Analyzer GolangCI-Lint
+
+This is a code generator tool that helps generate golang packages.
+Each of these packages export a single analysis.Analyzer variable which
+reflect the corresponding linter in `golangci-lint`
+
+Bazel's rules_go projects can then import this project and use the generated packages
+with the `nogo` static analysis framework.
+
+## Status
+
+Very much WIP.
+I am doing a quick brain dump here on a Friday to make sure that I don't forget about these on Monday.
+
+Also depending on https://github.com/golangci/golangci-lint/pull/2710 to get merged.
+Currently I am using a local go.mod replace
+
+## Design
+
+There are a few commands to implement:
+
+1. First we have to parse the golang package to find all linter constructor functions.
+   - For early stage, we can skip out constructors with parameters.
+   - Linters with custom configs may need manually crafted with the correct set of configuration.
+   - Possibly tell golangci-lint to use Analyzer's flagSet instead of config passing through constructor?
+   - For quick implementation, I am also using `go mod vendor` to download golangci-lint packages.
+   - Currently used go/ast packages but tree-sitter might be a better/faster approach?
+
+2. Implement a quick counting function to check if we can safely assume that all linters only have 1 analyzer.
+   It seems like that assumptions is true (for now).
+   This helps us avoid having to sort and iterate through the analyzers slice which is nice.
+
+3. Implement a code generator that create packages and the Go file using existing template.
+
+4. Generate starlark and nogo config JSON files programatically for easier consumption.
+
+Current TODOs: 3 and 4
+
+## References
+
+See https://github.com/sluongng/staticcheck-codegen which followed similar approach to expose staticcheck analyzers to nogo.
--- main.go
+++ main.go
@@ -1,7 +1,145 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"log"
+	"os"
+	"sort"
+	"strings"
+
+	"github.com/golangci/golangci-lint/pkg/golinters"
+	"github.com/golangci/golangci-lint/pkg/golinters/goanalysis"
+)
+
+var (
+	template = `# Generated file, do not modify
+package {{ .PackageName }}
+
+import (
+	"github.com/golangci/golangci-lint/pkg/golinters"
+)
+
+// Assuming that there is only 1 analyzer for each linter
+var Analyzer = golinters.{{ .FuncName }}.GetAnalyzers()[0]`
+
+	linters = []func() *goanalysis.Linter{
+		golinters.NewAsciicheck,
+		golinters.NewBodyclose,
+		golinters.NewContainedCtx,
+		golinters.NewContextCheck,
+		golinters.NewDeadcode,
+		golinters.NewDepguard,
+		golinters.NewDogsled,
+		golinters.NewDupl,
+		golinters.NewDurationCheck,
+		golinters.NewErrName,
+		golinters.NewErrcheck,
+		golinters.NewExportLoopRef,
+		golinters.NewForbidigo,
+		golinters.NewForceTypeAssert,
+		golinters.NewFunlen,
+		golinters.NewGoHeader,
+		golinters.NewGoPrintfFuncName,
+		golinters.NewGochecknoglobals,
+		golinters.NewGochecknoinits,
+		golinters.NewGocognit,
+		golinters.NewGoconst,
+		golinters.NewGocritic,
+		golinters.NewGocyclo,
+		golinters.NewGodot,
+		golinters.NewGodox,
+		golinters.NewGoerr113,
+		golinters.NewGofmt,
+		golinters.NewGofumpt,
+		golinters.NewGoimports,
+		golinters.NewGolint,
+		golinters.NewGomodguard,
+		golinters.NewIneffassign,
+		golinters.NewInterfacer,
+		golinters.NewLLL,
+		golinters.NewMakezero,
+		golinters.NewMaligned,
+		golinters.NewMisspell,
+		golinters.NewNakedret,
+		golinters.NewNestif,
+		golinters.NewNilErr,
+		golinters.NewNoLintLint,
+		golinters.NewNoctx,
+		golinters.NewParallelTest,
+		golinters.NewPrealloc,
+		golinters.NewPromlinter,
+		golinters.NewRowsErrCheck,
+		golinters.NewSQLCloseCheck,
+		golinters.NewScopelint,
+		golinters.NewStructcheck,
+		golinters.NewTparallel,
+		golinters.NewTypecheck,
+		golinters.NewUnconvert,
+		golinters.NewUnparam,
+		golinters.NewVarcheck,
+		golinters.NewWSL,
+		golinters.NewWastedAssign,
+		golinters.NewWhitespace,
+	}
+)
 
 func main() {
-	fmt.Println("vim-go")
+	switch os.Args[2] {
+	case "fun":
+		printFuncNames()
+		break
+	case "count":
+		printCount()
+		break
+	case "generate":
+	default:
+		generate()
+		break
+	}
+}
+
+func generate() {
+	// TODO
+}
+
+func printFuncNames() {
+	packs, err := parser.ParseDir(token.NewFileSet(), "vendor/github.com/golangci/golangci-lint/pkg/golinters", nil, 0)
+	if err != nil {
+		log.Fatalf("Could not parse directory")
+	}
+
+	funcs := []string{}
+	for _, pack := range packs {
+		for _, file := range pack.Files {
+			for _, d := range file.Decls {
+				if fn, isFn := d.(*ast.FuncDecl); isFn {
+					// Function should have a 'New' prefix
+					// and take in zero param
+					// and only have 1 output
+					if strings.HasPrefix(fn.Name.Name, "New") &&
+						len(fn.Type.Params.List) == 0 &&
+						len(fn.Type.Results.List) == 1 {
+						funcs = append(funcs, fn.Name.Name)
+					}
+				}
+			}
+		}
+	}
+
+	sort.Strings(funcs)
+
+	for _, fun := range funcs {
+		fmt.Printf(`golinters.%s,
+`, fun)
+	}
+}
+
+func printCount() {
+	for _, fun := range linters {
+		linter := fun()
+		fmt.Printf("%s %d\n", linter.Name(), len(linter.GetAnalyzers()))
+	}
 }
